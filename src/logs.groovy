cd34eda33101c8fc52b360a1c6815427d9396ef4
Arthur <arthurlecocq@gmail.com>
2026-05-25 10:50:42 +0200

    Init

---
+++ src/logs.groovy
@@ -0,0 +1,38 @@
+package src
+
+class Colors {
+    static final String RESET   = "\033[0m"
+    static final String BOLD    = "\033[1m"
+    static final String BLACK   = "\033[30m"
+
+    static final String RED     = "\033[31m"
+    static final String GREEN   = "\033[32m"
+    static final String YELLOW  = "\033[33m"
+    static final String BLUE    = "\033[34m"
+    static final String ORANGE  = "\033[38;5;208m"
+
+    void info(String iMessage, String isTextBold=false){
+        String style = isTextBold ? BOLD : ""
+        println("${style}$${BLUE}[INFO] - ${iMessage}${RESET}")
+    }
+
+    void warning(String iMessage, String isTextBold=false){
+        String style = isTextBold ? BOLD : ""
+        println("${style}$${ORANGE}[WARNING] - ${iMessage}${RESET}")
+    }
+
+    void error(String iMessage, String isTextBold=false){
+        String style = isTextBold ? BOLD : ""
+        println("${style}$${RED}[ERROR] - ${iMessage}${RESET}")
+    }
+
+    void debug(String iMessage, String isTextBold=false){
+        String style = isTextBold ? BOLD : ""
+        println("${style}$${BLACK}[DEBUG] - ${iMessage}${RESET}")
+    }
+
+    void system(String iMessage, String isTextBold=false){
+        String style = isTextBold ? BOLD : ""
+        println("${style}$${YELLOW}[SYSTEM] - ${iMessage}${RESET}")
+    }
+}
