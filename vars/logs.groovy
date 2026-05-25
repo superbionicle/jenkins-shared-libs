1f5233207ba089652e700b88b6e6d7e239b44e80
Arthur <arthurlecocq@gmail.com>
2026-05-25 14:56:46 +0200

    logs

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -2,25 +2,25 @@ import utils.colors
 
 void info(String iMessage, boolean isTextBold=false){
         String style = isTextBold ? colors.BOLD : ""
-        println("${style}${colors.BLUE}[INFO] - ${iMessage}${colors.RESET}")
+        println("${style}${colors.BLUE}[INFO]\t\t - ${iMessage}${colors.RESET}")
     }
 
 void warning(String iMessage, boolean isTextBold=false){
     String style = isTextBold ? colors.BOLD : ""
-    println("${style}${colors.ORANGE}[WARNING] - ${iMessage}${colors.RESET}")
+    println("${style}${colors.ORANGE}[WARNING]\t\t - ${iMessage}${colors.RESET}")
 }
 
 void error(String iMessage, boolean isTextBold=false){
     String style = isTextBold ? colors.BOLD : ""
-    println("${style}${colors.RED}[ERROR] - ${iMessage}${colors.RESET}")
+    println("${style}${colors.RED}[ERROR]\t\t - ${iMessage}${colors.RESET}")
 }
 
 void debug(String iMessage, boolean isTextBold=false){
     String style = isTextBold ? colors.BOLD : ""
-    println("${style}${colors.BLACK}[DEBUG] - ${iMessage}${colors.RESET}")
+    println("${style}${colors.BLACK}[DEBUG]\t\t - ${iMessage}${colors.RESET}")
 }
 
 void system(String iMessage, boolean isTextBold=false){
     String style = isTextBold ? colors.BOLD : ""
-    println("${style}${colors.YELLOW}[SYSTEM] - ${iMessage}${colors.RESET}")
+    println("${style}${colors.YELLOW}[SYSTEM]\t\t - ${iMessage}${colors.RESET}")
 }
